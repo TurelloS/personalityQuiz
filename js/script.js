7c6b2903171a2e5eda319e939d1ad18634a663c3
TurelloS <StephenTurello23@gmail.com>
2025-11-26 13:37:59 -0600

    added listener

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 console.log("script.js connected!");
+
+let question = document.querySelectorAll(question-block)
+
+let buttons = document.querySelectorAll("#answer-btn");
+
+buttons.forEach(function(button){
+    button.addEventListener("click", function() {
+        console.log("button clicked!");
+    })
+})
